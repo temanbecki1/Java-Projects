23251f0ab043d20d9d5c1e402052f70acc582da0
Teman Beck <temanbeck@yahoo.com>
2021-08-19 21:32:25 -0400

    testInput logic added to Conversion class.

--- Conversion.java
+++ Conversion.java
@@ -2,6 +2,7 @@ import java.util.Stack;
 
 public class Conversion {
     String exp;                                                             //creates locale variable to copy user input from conversion button action
+    String reverseString = "";
     Stack<String> stack1;                                                   //creates empty stack
     
 
@@ -11,7 +12,6 @@ public class Conversion {
 
     public Conversion(String expression){
         this.exp = expression;
-
     }
 
     public void testInput(String expression){
@@ -20,9 +20,41 @@ public class Conversion {
         System.out.println("The input is " + exp);
 
         if(exp.length() >=0){                                               //test to see if length is greater than zero
-            System.out.println("The length of input is " + exp.length());
+            System.out.println("There are a total of  " + exp.length() + " index in this String");
+
+            for(int i = exp.length()-1; i>=0; i--){
+                //reverseString = reverseString + exp.charAt(i);
+
+                //char testIndex = exp.charAt(i);
+
+                switch(exp.charAt(i)){
+                    case '*':
+                    case '/':
+                    case '+':
+                    case '-':
+
+                    System.out.println("Character at index " + i + " is a " + exp.charAt(i));
+                    break;
+
+                    default : 
+
+                    System.out.println("Character at index " + i + " is an operand. ");                 
+
+                    break;
+
+
+                }
+
+            }
         }
+
+        //System.out.println("testing reverse :" + reverseString);
+        
     }
+
+    // public String getResult(){
+    //     return reverseString;
+    // }
     
 
     
